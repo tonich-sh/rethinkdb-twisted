c6b88ac41fd216133830750cffcb790422e266d4
Anton Schur <anton_s@florist.ru>
2014-09-23 11:14:22 +0400

    Cleanups

--- rethinkdb/twisted/__init__.py
+++ rethinkdb/twisted/__init__.py
@@ -1,3 +1,4 @@
+# Copyright 2014 Anton Schur
 
 import logging
 import struct
--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@
 from setuptools import setup
 
 setup(
-    name="rethinkdb-twisted"
-    , zip_safe=True
-    , version="0.0.1"
-    , description="This package provides the Twisted connector for the Python\
-driver library for the RethinkDB database server."
-    , url="https://github.com/"
-    , maintainer="Anton Schur"
-    , maintainer_email="tonich.sh@gmail.com"
-    , packages=['rethinkdb.twisted']
-    , install_requires=['rethinkdb>=1.13.0', 'Twisted>=14.0.0'],
+    name="rethinkdb-twisted",
+    zip_safe=True,
+    version="0.0.1",
+    description="This package provides the Twisted connector for the Python\
+driver library for the RethinkDB database server.",
+    url="https://github.com/",
+    maintainer="Anton Schur",
+    maintainer_email="tonich.sh@gmail.com",
+    license='Apache-2.0',
+    packages=['rethinkdb.twisted'],
+    install_requires=['rethinkdb>=1.13.0', 'Twisted>=14.0.0'],
 )
